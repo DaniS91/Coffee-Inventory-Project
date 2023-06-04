d65e56d19ffe31b5884eaaa9aa2f1d29abb4fb87
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-04 10:02:54 -0700

    Add simple header component and import to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
   return (
     <React.Fragment>
+      <Header />
       <h1>Coffee Inventory</h1>
       <h3>130 pounds</h3>
       <hr/>
