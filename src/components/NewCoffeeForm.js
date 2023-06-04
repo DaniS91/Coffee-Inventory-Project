b9d041695af622935ed09b7158eac3d9d0c143cf
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-04 11:06:27 -0700

    start work on form for adding new coffee, with event handler for submitting form

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -1,9 +1,40 @@
 import React from "react";
 
 function NewCoffeeForm(props){
+
+  function handleNewCoffee(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.origin.value);
+    console.log(event.target.roast.value);
+    console.log(event.target.price.value);
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewCoffee}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Coffee Name' />
+        <br></br>
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin' />
+        <br></br>
+        <input
+          type='text'
+          name='roast'
+          placeholder='Roast' />
+        <br></br>
+        <input
+          type='number'
+          name='price'
+          placeholder='Price' />
+        <br></br>
+        <button type='submit'>Submit</button>
+      </form>
     </React.Fragment>
   );
 }
