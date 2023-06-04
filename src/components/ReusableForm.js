1207790ff173ece6724d4d537c192e2b1a136102
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-04 12:54:09 -0700

    Add reusable form for handling creation and updating

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+      <input
+          type='text'
+          name='name'
+          placeholder='Coffee Name' />
+        <br></br>
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin' />
+        <br></br>
+        <input
+          type='text'
+          name='roast'
+          placeholder='Roast' />
+        <br></br>
+        <input
+          type='number'
+          name='price'
+          placeholder='Price' />
+        <br></br>
+        <button type='submit'>Submit</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
