1b3357bb5808c5d1e9f5936eaf02ef484c41c429
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-04 10:48:16 -0700

    add skeleton for inventorycontrol component that will control all other compontents and state

---
+++ src/components/InventoryControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewCoffeeForm from './NewCoffeeForm';
+import CoffeeList from './CoffeeList';
+
+class InventoryControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default InventoryControl;
