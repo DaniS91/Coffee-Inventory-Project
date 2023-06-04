e66c466d3fd47bee77ad6e23869b047c9533f18f
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-04 10:01:01 -0700

    add babel plugin and set up component folder

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Coffee Inventory</h1>
+      <h3>130 pounds</h3>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
