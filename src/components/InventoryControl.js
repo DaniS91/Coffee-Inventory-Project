49a52ebeebe9fbeb3d4a5d52e2e97ec1d5b013e9
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-04 10:52:46 -0700

    begin adding state to Inventory Control, WIP

--- src/components/InventoryControl.js
+++ src/components/InventoryControl.js
@@ -6,12 +6,22 @@ class InventoryControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisible: false
+    };
   }
 
   render(){
+
+    let currentState = null;
+    if (this.state.formVisible) {
+      currentState = <NewCoffeeForm />
+    } else {
+      currentState = <CoffeeList />
+    }
     return (
       <React.Fragment>
+        {currentState}
       </React.Fragment>
     );
   }
