b0f751b778fd6000a7b27132a86d38fb8a6ccf69
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-06-04 10:44:18 -0700

    test and confirm proptypes in coffee and looping in coffeelist

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Coffee(props){
   return (
@@ -7,9 +8,18 @@ function Coffee(props){
       <p>Origin: {props.origin}</p>
       <p>{props.roast}</p>
       <p>{props.price}</p>
+      <p>{props.inventory}</p>
       <hr/>
     </React.Fragment>
   );
+}
+
+Coffee.propTypes = {
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  roast: PropTypes.string,
+  price: PropTypes.string,
+  inventory: PropTypes.string
 }
 
 export default Coffee;
--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -1,22 +1,35 @@
 import React from "react";
 import Coffee from "./Coffee";
 
+
+const mainCoffeeList = [
+  {
+    name: "Bridge Blend",
+    roast: "Light Roast",
+    origin: "Indonesia, Africa",
+    price: "$16.99",
+    inventory: "130"
+  },
+  {
+    name: "Colombia direct trade",
+    roast: "Medium Roast",
+    origin: "Colombia",
+    price: "$19.99",
+    inventory: "130"
+  }
+]
 function CoffeeList(){
   return (
     <>
-    <Coffee 
-      name="Bridge Blend"
-      roast="Light Roast"
-      origin="Indonesia, Africa"
-      price="$16.99"
-      />
-      <Coffee 
-      name="Colombia direct trade"
-      roast="Medium Roast"
-      origin="Colombia"
-      price="$19.99"
-      />
-      </>
+    <hr/>
+    {mainCoffeeList.map((coffee, index) =>
+      <Coffee name={coffee.name}
+        origin={coffee.origin}
+        roast={coffee.roast}
+        price={coffee.price}
+        inventory={coffee.inventory}/>
+    )}
+    </>
   );
 }
 
